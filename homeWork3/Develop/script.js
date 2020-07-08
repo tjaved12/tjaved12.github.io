0bb6ffa431e62d3d9f831f8b969ba894c36f0808
TJ <tahmeenaowais@yahoo.com>
2020-07-08 14:51:49 -0400

    fix

--- homeWork3/Develop/script.js
+++ homeWork3/Develop/script.js
@@ -1,11 +1,11 @@
 
 var max = prompt("How many characters you want in your password?");
-var i = prompt("You want to have numerals in your password?yes or no");
-var j = prompt("You want to have special CHR in your password?yes or no");
-var k = prompt("You want to have lower case alphabets in your password?yes or no ");
-var l = prompt("You want to have uppercase alphabets in your password?yes or no");
+var i = confirm("You want to have numerals in your password?");
+var j = confirm("You want to have special CHR in your password?");
+var k = confirm("You want to have lower case alphabets in your password?");
+var l = confirm("You want to have uppercase alphabets in your password?");
 
-var numeral ='0123456789';
+var numeral = [0,1,2,3,4,5,6,7,8,9];
 var special_Chr = '!#$%&*';
 var lower_alphabet = 'abcdefghijklmnopqrstuvwxyz';
 var upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -14,9 +14,9 @@ var characterPool = [];
 
 
 
-if (i === 'Yes') {
+if (i === true) {
  pass += numeral[Math.floor(Math.random()*numeral.length)];
- characterPool.concat(numeral);
+ characterPool += numeral;
 }
  else  {
   numeral = ('');
@@ -32,25 +32,25 @@ if (i === 'Yes') {
  // numeral = ('');
  // }
 
-if (j==="yes"){
+if (j === true){
   pass += special_Chr[Math.floor(Math.random()*special_Chr.length)];
-characterPool.concat(special_Chr);
+characterPool  += special_Chr;
 }
 else {
  special_Chr = ('');
 }
 
-if (k==="yes"){
+if (k === true){
   pass += lower_alphabet[Math.floor(Math.random()*lower_alphabet.length)]; 
- characterPool.concat(lower_alphabet);
+ characterPool += lower_alphabet;
 }
  else{
 lower_alphabet= ('');
 }
  
-  if (l==="yes"){
+  if (l === true){
     pass += upper_alphabet[Math.floor(Math.random()*upper_alphabet.length)];
-    characterPool.concat(upper_alphabet);
+    characterPool += upper_alphabet;
   } 
    else {
  upper_alphabet= ('');
@@ -58,13 +58,20 @@ lower_alphabet= ('');
  
   //Then finish building the password
 
-function randomPassword(length){
-while(pass.length < max) {
+function randomPassword(){
+for(z = pass.length; z < max; z++) {
+
+  pass += pass [Math.floor(Math.random()*pass.length)];
+  myform.row_password.value = pass;
  
- pass += characterPool[Math.floor(Math.random()*characterPool.length)];
- myform.row_password.value = pass;
+
 }
 }
+
+console.log(pass[0])
+console.log(pass[1])
+console.log(pass[2])
+console.log(pass[3])
   //function randomPassword(length){
    // pass= (numeral+special_Chr+lower_alphabet+Upper_alphabet);
   //for (pass=0;pass("")< max(""); pass++);
